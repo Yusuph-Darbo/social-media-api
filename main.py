a1aa13100db5c00d4e4893838696c248ed0352ff
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-03-24 14:45:49 +0000

    basic validation with pydantic

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+
+class Post(BaseModel):
+    title: str
+    desc: str
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "A list of posts"}
+
+
+@app.post("/posts")
+def create_post(payload: Post):
+    print(payload)
+    return {"message": f"success, title : {payload.title}"}
