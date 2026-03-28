8b16135afb25a6b952893575863a0a63899a07a6
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-03-28 18:32:54 +0000

    installed and implemented sqlalchemy

--- app/main.py
+++ app/main.py
@@ -5,8 +5,21 @@ from typing import Optional
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
