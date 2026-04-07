3a7bcd8b786eab40d5ec937ad117be356cd91f2c
Yusuph Darbo <yusuphdarbo@gmail.com>
2026-04-07 15:50:20 +0100

    using env values

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routes import post, user, auth
@@ -10,26 +7,6 @@ from .routes import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="yusuphdarbo",
-            user="postgres",
-            password="rootUser",
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        # Have to specify which schema to query
-        cursor.execute("SET search_path TO social_media_api;")
-        print("Database connection was successful")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
 
 app.include_router(post.router)
 app.include_router(user.router)
